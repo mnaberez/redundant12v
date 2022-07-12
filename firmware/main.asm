13f0a86dc6466fe28b09ed510a78bd4f731d2e7f
Mike Naberezny <mike@naberezny.com>
2022-07-11 22:29:42 -0700

    Use ASAVR high/low byte operators

--- firmware/main.asm
+++ firmware/main.asm
@@ -40,22 +40,22 @@ reset:
     cpi ZH, >(INTERNAL_SRAM_END+1)
     brne 1$
 
-  	;Initialize stack pointer
-    ldi r16, INTERNAL_SRAM_END & 0xFF
+    ;Initialize stack pointer
+    ldi r16, <INTERNAL_SRAM_END
     out CPU_SPL, r16
-    ldi r16, INTERNAL_SRAM_END >> 8
+    ldi r16, >INTERNAL_SRAM_END
     out CPU_SPH, r16
 
-  	;Disable clock prescaler
-  	clr r16
-  	ldi r17, CPU_CCP_IOREG_gc
-  	sts CPU_CCP, r17					        ;Unlock Protected I/O Registers
-  	sts CLKCTRL_MCLKCTRLB, r16			  ;Disable clock prescaler
+    ;Disable clock prescaler
+    clr r16
+    ldi r17, CPU_CCP_IOREG_gc
+    sts CPU_CCP, r17                  ;Unlock Protected I/O Registers
+    sts CLKCTRL_MCLKCTRLB, r16        ;Disable clock prescaler
 
-  	;Switch to external 1.8432 MHz oscillator
-  	ldi r16, CLKCTRL_CLKSEL_EXTCLK_gc	;EXTCLK 1.8432 MHz external clock
-  	sts CPU_CCP, r17					        ;Unlock Protected I/O Registers
-  	sts CLKCTRL_MCLKCTRLA, r16			  ;Use EXTCLK for main clock
+    ;Switch to external 1.8432 MHz oscillator
+    ldi r16, CLKCTRL_CLKSEL_EXTCLK_gc  ;EXTCLK 1.8432 MHz external clock
+    sts CPU_CCP, r17                  ;Unlock Protected I/O Registers
+    sts CLKCTRL_MCLKCTRLA, r16        ;Use EXTCLK for main clock
 
     rcall gpio_init
     rcall uart_init
@@ -182,10 +182,10 @@ uart_init:
     ldi r16, 0x03
     sts USART0_BAUDH, r16
 
-  	;USART0.CTRLC = USART_CMODE_ASYNCHRONOUS_gc /* Asynchronous Mode */
-  	;	 | USART_CHSIZE_8BIT_gc /* Character size: 8 bit */
-  	;	 | USART_PMODE_DISABLED_gc /* No Parity */
-  	;	 | USART_SBMODE_1BIT_gc; /* 1 stop bit */
+    ;USART0.CTRLC = USART_CMODE_ASYNCHRONOUS_gc /* Asynchronous Mode */
+    ;   | USART_CHSIZE_8BIT_gc /* Character size: 8 bit */
+    ;   | USART_PMODE_DISABLED_gc /* No Parity */
+    ;   | USART_SBMODE_1BIT_gc; /* 1 stop bit */
     ldi r16, USART_NORMAL_CMODE_ASYNCHRONOUS_gc | USART_NORMAL_CHSIZE_8BIT_gc | USART_NORMAL_PMODE_DISABLED_gc | USART_NORMAL_SBMODE_1BIT_gc
     sts USART0_CTRLC, r16
 
